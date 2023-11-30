370b97ca8db43af2c1d49984eee4576c65dd9704
Jeff Yang <n2ya@duck.com>
2023-11-30 12:33:26 +0800

    chore: repo setup (#5)

--- src/objects/space_entity.ts
+++ src/objects/space_entity.ts
@@ -1,65 +1,59 @@
-import { Vector3 } from "structures/vector";
+import { Vector3 } from '../structures/vector';
 
 /**
  * Represents an object in the 3D space that has a position and a direction
  */
 export abstract class SpaceEntity {
-    /**
-     * World coordinates
-     */
-    private _position: Vector3 = Vector3.zero;
+	/**
+	 * World coordinates
+	 */
+	#position: Vector3 = Vector3.zero;
+	/**
+	 * Unit vector which represents a direction at which object looks forward
+	 */
+	#direction: Vector3 = Vector3.forward;
 
-    public get position() {
-        return this._position;
-    }
+	get position() {
+		return this.#position;
+	}
 
-    public setPosition(position: Vector3) {
-        this._position = position;
-    } 
+	setPosition(position: Vector3) {
+		this.#position = position;
+	}
 
-    // ===
+	get direction() {
+		return this.#direction;
+	}
 
-    /**
-     * Unit vector which represents a direction at which object looks forward
-     */
-    private _direction: Vector3 = Vector3.forward;
+	setDirection(direction: Vector3) {
+		this.#direction = direction;
+	}
 
-    public get direction() {
-        return this._direction;
-    }
+	/**
+	 * Rotates object according to right hand rule
+	 * @param direction normal vector
+	 * @param angle angle in radians
+	 */
+	rotate(direction: Vector3, angle: number): void {
+		this.#direction.rotate(direction, angle);
+	}
 
-    public setDirection(direction: Vector3) {
-        this._direction = direction;
-    }
+	/**
+	 * Translates object relative to its direction
+	 */
+	translate(delta: Vector3): void {
+		const angle = Vector3.getAngleBetween(this.direction, Vector3.forward);
+		const cross = Vector3.cross(this.direction, Vector3.forward);
+		const normal = cross.unit();
 
-    // ===
+		if (cross.sqrMagnitude() !== 0) {
+			delta.rotate(normal, angle);
+		} else if (angle == Math.PI) {
+			delta.multiply(-1);
+		}
 
-    /**
-     * Rotates object according to right hand rule
-     * @param direction normal vector
-     * @param angle angle in radians
-     */
-    rotate(direction: Vector3, angle: number): void {
-        this._direction.rotate(direction, angle);
-    }
-    
-    /**
-     * Translates object relative to its direction
-     */
-    translate(delta: Vector3): void {
-        const angle = Vector3.getAngleBetween(this.direction, Vector3.forward);
-        const cross = Vector3.cross(this.direction, Vector3.forward);
-        const normal = cross.unit();
-        
-        if(cross.sqrMagnitude() !== 0) {
-            delta.rotate(normal, angle);    
-        }
-        else if(angle == Math.PI) {
-            delta.multiply(-1);
-        }
+		// console.log('normal: ', normal, ']\nangle: ', angle * 180/Math.PI, '\ndelta: ', delta, '\nposition: ', this.position);
 
-        // console.log('normal: ', normal, ']\nangle: ', angle * 180/Math.PI, '\ndelta: ', delta, '\nposition: ', this.position);
-
-        this.setPosition(Vector3.add(this.position, delta));
-    }
+		this.setPosition(Vector3.add(this.position, delta));
+	}
 }
--- src/structures/polygon.ts
+++ src/structures/polygon.ts
@@ -1,23 +1,23 @@
-import { Vector3 } from "./vector";
+import { Vector3 } from './vector';
 
 export class Polygon {
-    /**
-     * Should contain only 3 vertexes
-     */
-    public vertexes: Vector3[];
+	/**
+	 * Should contain only 3 vertexes
+	 */
+	vertexes: Vector3[];
 
-    constructor(...vertexes: Vector3[]) {
-        this.vertexes = vertexes;
-    }
+	constructor(...vertexes: Vector3[]) {
+		this.vertexes = vertexes;
+	}
 
-    map(func: (vertex: Vector3) => Vector3): Polygon {
-        const { length } = this.vertexes;
-        const vertexes: Vector3[] = new Array(length);
-        
-        for(let i=0; i < length; i++) {
-            vertexes[i] = func(this.vertexes[i].clone());
-        }
+	map(func: (vertex: Vector3) => Vector3): Polygon {
+		const { length } = this.vertexes;
+		const vertexes: Vector3[] = new Array(length);
 
-        return new Polygon(...vertexes);
-    }
+		for (let i = 0; i < length; i++) {
+			vertexes[i] = func(this.vertexes[i]!.clone());
+		}
+
+		return new Polygon(...vertexes);
+	}
 }
