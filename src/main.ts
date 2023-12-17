3a1250248c2cdfcb39e12b768bb46203c83a9450
ydcjeff <n2ya@duck.com>
2023-12-17 21:20:00 +0800

    lint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Engine } from './core/engine';
 import { CubeScene } from './scenes/cube-scene';
-import { UrbanScene } from './scenes/urban-scene';
 import { TestScene } from './scenes/test-scene';
+import { UrbanScene } from './scenes/urban-scene';
 
 const canvas = document.querySelector('canvas')!;
 canvas.width = canvas.offsetWidth * 2;
