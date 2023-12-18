53eb46f32bd5225dabde4b4282a76b0cf6d3fce9
ydcjeff <n2ya@duck.com>
2023-12-19 00:05:13 +0800

    fix: increase fov +/-

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ const engine = new Engine(canvas);
 (async () => {
 	// await engine.setScene(AxisScene);
 	// await engine.setScene(RotatingScene);
-	// await engine.setScene(CameraRotatingScene);
+	await engine.setScene(CameraRotatingScene);
 	// await engine.setScene(ScaleScene);
 	// await engine.setScene(TranslationScene);
 	// await engine.setScene(UrbanScene);
